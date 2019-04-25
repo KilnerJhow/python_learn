a9626f9dd29b0ee0ee31fa6ec62de967b322ef72
KilnerJhow <jkss@cin.ufpe.br>
2019-04-25 12:42:08 -0300

    First lessons in python

---
+++ hello_world.py
@@ -0,0 +1,23 @@
+#Comments in python
+#Is a identation sensitive language
+
+print ("Hello World") #Print in terminal
+
+#Strings can be used with single or double quotes
+mystring = 'hello'
+print (mystring)
+
+mystring = "hello"
+print (mystring)
+
+#The difference is whe using apostrophes
+
+mystring = 'one aphostrophe \'' #need the \ before apostrophe
+print (mystring)
+
+mystring = "one aphostrophe '" #dont need anything
+print mystring
+
+#You can make various assignments in one time
+a,b = 3,4
+print (a,b)
